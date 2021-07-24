9eb40757285011b3fb0883dd5e27a0d3d5af077b
Patrick Johnson <pjpuzzler@gmail.com>
2021-07-23 23:39:05 -0400

    c

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,7 @@
-// This is a complete remake of niklasf's python-chess in C++. The original version can be found here: https://github.com/niklasf/python-chess.
+/*
+This is a complete remake of niklasf's python-chess in C++
+The original version can be found here: https://github.com/niklasf/python-chess
+*/
 
 #include <string>
 #include <unordered_map>
@@ -18,14 +21,14 @@ const PieceType PIECE_TYPES[] = {1, 2, 3, 4, 5, 6}, PAWN = 1, KNIGHT = 2, BISHOP
 char PIECE_SYMBOLS[] = {'\0', 'p', 'n', 'b', 'r', 'q', 'k'};
 string PIECE_NAMES[] = {"", "pawn", "knight", "bishop", "rook", "queen", "king"};
 
-char pieceSymbol(PieceType pieceType)
+char piece_symbol(PieceType piece_type)
 {
-    return PIECE_SYMBOLS[pieceType];
+    return PIECE_SYMBOLS[piece_type];
 }
 
-string pieceName(PieceType pieceType)
+string piece_name(PieceType piece_type)
 {
-    return PIECE_NAMES[pieceType];
+    return PIECE_NAMES[piece_type];
 }
 
 const unordered_map<char, string> UNICODE_PIECE_SYMBOLS = {
@@ -94,6 +97,58 @@ const int STATUS_RACE_OVER = Status::RACE_OVER;
 const int STATUS_RACE_MATERIAL = Status::RACE_MATERIAL;
 const int STATUS_TOO_MANY_CHECKERS = Status::TOO_MANY_CHECKERS;
 const int STATUS_IMPOSSIBLE_CHECK = Status::IMPOSSIBLE_CHECK;
+
+class Termination
+{
+    // Enum with reasons for a game to be over.
+
+    const static int CHECKMATE = 1;
+    // See :func:`chess.Board.is_checkmate()`.
+    const static int CSTALEMATE = 2;
+    // See :func:`chess.Board.is_stalemate()`.
+    const static int CINSUFFICIENT_MATERIAL = 3;
+    // See :func:`chess.Board.is_insufficient_material()`.
+    const static int CSEVENTYFIVE_MOVES = 4;
+    // See :func:`chess.Board.is_seventyfive_moves()`.
+    const static int CFIVEFOLD_REPETITION = 5;
+    // See :func:`chess.Board.is_fivefold_repetition()`.
+    const static int CFIFTY_MOVES = 6;
+    // See :func:`chess.Board.can_claim_fifty_moves()`.
+    const static int CTHREEFOLD_REPETITION = 7;
+    // See :func:`chess.Board.can_claim_threefold_repetition()`.
+    const static int CVARIANT_WIN = 8;
+    // See :func:`chess.Board.is_variant_win()`.
+    const static int CVARIANT_LOSS = 9;
+    // See :func:`chess.Board.is_variant_loss()`.
+    const static int CVARIANT_DRAW = 10;
+    // See :func:`chess.Board.is_variant_draw()`.
+};
+
+class Outcome
+{
+    /*
+    Information about the outcome of an ended game, usually obtained from
+    :func:`chess.Board.outcome()`.
+    */
+
+public:
+    Termination termination;
+    Color winner;
+
+    Outcome(Termination termination, Color winner)
+    {
+        termination = termination;
+        // The reason for the game to have ended.
+        winner = winner;
+        // The winning color or ``None`` if drawn.
+    }
+
+    string result()
+    {
+        // Returns ``1-0``, ``0-1`` or ``1/2-1/2``.
+        return winner == NULL ? "1/2-1/2" : (winner ? "1-0" : "0-1");
+    }
+};
 
 int main()
 {
