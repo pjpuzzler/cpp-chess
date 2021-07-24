a2684c0fdde3308e9c7de38ae1bb853e773da974
Patrick Johnson <pjpuzzler@gmail.com>
2021-07-24 14:25:22 -0400

    c

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-if (board_fen.empty())
-        //     this->_clear_board();
-        // else if (board_fen == STARTING_BOARD_FEN)
-        //     this->_reset_board();
-        // else
-        //     this->_set_board_fen(board_fen);
