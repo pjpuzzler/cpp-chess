ed911c5e1e73de4c217500bda8f7f5f0f04c872d
Patrick Johnson <pjpuzzler@gmail.com>
2021-07-31 15:25:57 -0400

    c

--- chess.h
+++ chess.h
@@ -1,31 +1,42 @@
 #include <string>
 #include <optional>
 #include <variant>
+#include <functional>
+#include <vector>
 
-namespace std {
+namespace chess {
     typedef unsigned long Bitboard;
     class BaseBoard {
     public:
-        BaseBoard(const optional<string> &);
-        void apply_transform(const function<Bitboard(Bitboard)> &);
+        BaseBoard(const std::optional<std::string> &);
+        void apply_transform(const std::function<Bitboard(Bitboard)> &);
         void apply_mirror();
         Bitboard pawns, knights, bishops, rooks, queens, kings, occupied_co[2], occupied, promoted;
         void set_chess960_pos(int);
     };
     class SquareSet;
     typedef int Square;
-    typedef variant<Bitboard, SquareSet, vector<Square>> IntoSquareSet;
+    typedef std::variant<Bitboard, SquareSet, std::vector<Square>> IntoSquareSet;
     class SquareSet {
     public:
         SquareSet(const IntoSquareSet &);
     };
     typedef bool Color;
+    class Board;
+    class LegalMoveGenerator {
+    public:
+        LegalMoveGenerator(const Board &board);
+    };
+    class PseudoLegalMoveGenerator {
+    public:
+        PseudoLegalMoveGenerator(const Board &);
+    };
     class Board {
     public:
         Bitboard pawns, knights, bishops, rooks, queens, kings, occupied_co[2], occupied, promoted, castling_rights;
         Color turn;
-        optional<Square> ep_square;
+        std::optional<Square> ep_square;
         int halfmove_clock, fullmove_number;
-        Board(const optional<string> &, bool);
+        Board(const std::optional<std::string> &, bool);
     };
 }
